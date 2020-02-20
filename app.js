7e6165ca01beeef2b6a15360b0ef6644fae4b694
Ayush23Dash <ayushshanker23@gmail.com>
2020-02-20 18:38:00 +0530

    Added home page

---
+++ app.js
@@ -0,0 +1,18 @@
+// jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+app.get("/",function(req,res)
+{
+  res.render("home");
+});
+
+app.listen(3000,function()
+{
+  console.log("Server is running on port 3000");
+});
